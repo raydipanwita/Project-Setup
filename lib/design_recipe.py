33c983ef3c50be5e8eff05b8386e5173bd2c947a
Dipanwita <rite2dipa@gmail.com>
2023-11-16 10:31:38 +0000

    Counts words

--- lib/design_recipe.py
+++ lib/design_recipe.py
@@ -1,8 +1,8 @@
 def design_recipe(text):
-    
-    words_per_min = 200
-    time_to_read = text/words_per_min
-    return time_to_read
+    words = text.split()
+    wordcount = len(words)
+    return wordcount / 200
+
 
 def design_recipe2(word):
     if word[0].isupper() and word[-1] in {"." ,"!" , "?" }: 
